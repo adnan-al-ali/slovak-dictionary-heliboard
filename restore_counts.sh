6e0fb32d26ee6a5a2c3450ce7c9e271b7035e589
alali <alali@leda.n.mff.cuni.cz>
2026-05-22 10:30:09 +0200

    Implemented manually excluding short words that get through filtering

--- restore_counts.sh
+++ restore_counts.sh
@@ -3,6 +3,9 @@
 filtered="filtered.txt"
 spell="spell_checked.txt"
 
+# sort undiacritized words that get through the filtering
+forbidden=("ze" "uz" "co" "su" "ci" "az" "ked" "nas" "nam" "tym" "nic" "den" "vas" "cas" "mna" "nom" "nez" "ine" "kym" "sam" "mam" "ist" "par" "cim" "nou" "ano" "pan" "moj" "ake" "aka" "aky" "dat" "muz" "ved")
+
 exec 3< "$spell"
 
 IFS= read -r spell_word <&3 || exit 0
@@ -11,12 +14,17 @@ while IFS= read -r line; do
     # split into word and count
     word=${line%%$'\t'*}
     count=${line#*$'\t'}
+    if [[ ! " ${forbidden[*]} " =~ [[:space:]]${word,,}[[:space:]] ]]; then
+        if [[ "$word" == "$spell_word" ]] || (( $count > 1000 )); then
+            printf ' word=%s,f=%s\n' "$word" "$count"
 
-    if [[ "$word" == "$spell_word" ]] || (($count > 1000)); then
-        printf ' word=%s,f=%s\n' "$word" "$count"
-
+            if ! IFS= read -r spell_word <&3; then
+                break
+            fi
+        fi
+    else
         if ! IFS= read -r spell_word <&3; then
-            break
+                break
         fi
     fi
 done < "$filtered"
